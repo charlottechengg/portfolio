08b6fec6b506e063e65f2d926ec816f116096792
Manyi Cheng <chengm33@mcmaster.ca>
2022-02-22 00:54:12 -0500

    update intro and appbar

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import Intro from './components/Intro';
+import './App.css';
+import Appbar from './components/Appbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Appbar/>
+      <Intro/>
+
+    </div>
+  );
+}
+
+export default App;
